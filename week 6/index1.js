bada9676b9a649c961d2e8b0bae7b44d08f621a8
alaa521 <76689888+alaa521@users.noreply.github.com>
2021-03-24 00:12:55 +0200

    Delete week 6 directory

--- week 6/index1.js
+++
@@ -1,9 +0,0 @@
-let bttn =document.getElementById("changeStyle");
-bttn.addEventListener("click", function changeStyle(element){
-    element.preventDefault();
-    document.getElementById("text");
-    text.style.fontSize ="35px";
-    text.style.backgroundColor ="#565adc";
-    text.style.color ="#ff8080";
-    text.style.padding ="10px";
-});
